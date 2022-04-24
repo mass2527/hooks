b2cfc9088503c638ec9b7beb4f9740540e0f1ac1
mass2527 <nzp01@naver.com>
2022-04-24 17:16:33 +0900

    useToggle hooks test 코드 수정

--- src/hooks/useToggle/useToggle.test.ts
+++ src/hooks/useToggle/useToggle.test.ts
@@ -5,58 +5,60 @@ import { useToggle } from "./useToggle";
 // renderHook mutates the value of current when updates happen so you cannot destructure its values
 // as the assignment will make a copy locking into the value at that time.
 
-it("should be initialized with false if no argument is provided", () => {
-  const { result } = renderHook(() => useToggle());
+describe("useToggle", () => {
+  it("should be initialized with false if no argument is provided", () => {
+    const { result } = renderHook(() => useToggle());
 
-  expect(result.current.isActive).toBe(false);
-});
-
-it("should be initialized with initial value", () => {
-  const { result } = renderHook(() => useToggle(true));
-
-  expect(result.current.isActive).toBe(true);
-});
-
-it("should be toggled", () => {
-  const { result } = renderHook(() => useToggle());
-
-  act(() => result.current.onToggle());
-  expect(result.current.isActive).toBe(true);
-});
-
-it("should be set on", () => {
-  const { result } = renderHook(() => useToggle());
-
-  act(() => result.current.setOn());
-  expect(result.current.isActive).toBe(true);
-});
-
-it("should be set off", () => {
-  const { result } = renderHook(() => useToggle(true));
-
-  act(() => result.current.setOff());
-  expect(result.current.isActive).toBe(false);
-});
-
-it("should be reset", () => {
-  const { result } = renderHook(() => useToggle(true));
-
-  act(() => {
-    result.current.setOff();
-    result.current.onReset();
+    expect(result.current.isActive).toBe(false);
   });
-  expect(result.current.isActive).toBe(true);
-});
 
-it("should reset isActive to updated initial value", () => {
-  const { result, rerender } = renderHook(
-    ({ initialValue }) => useToggle(initialValue),
-    {
-      initialProps: { initialValue: false },
-    }
-  );
+  it("should be initialized with initial value", () => {
+    const { result } = renderHook(() => useToggle(true));
 
-  rerender({ initialValue: true });
-  act(() => result.current.onReset());
-  expect(result.current.isActive).toBe(true);
+    expect(result.current.isActive).toBe(true);
+  });
+
+  it("should be toggled", () => {
+    const { result } = renderHook(() => useToggle());
+
+    act(() => result.current.onToggle());
+    expect(result.current.isActive).toBe(true);
+  });
+
+  it("should be set on", () => {
+    const { result } = renderHook(() => useToggle());
+
+    act(() => result.current.setOn());
+    expect(result.current.isActive).toBe(true);
+  });
+
+  it("should be set off", () => {
+    const { result } = renderHook(() => useToggle(true));
+
+    act(() => result.current.setOff());
+    expect(result.current.isActive).toBe(false);
+  });
+
+  it("should be reset", () => {
+    const { result } = renderHook(() => useToggle(true));
+
+    act(() => {
+      result.current.setOff();
+      result.current.onReset();
+    });
+    expect(result.current.isActive).toBe(true);
+  });
+
+  it("should reset isActive to updated initial value", () => {
+    const { result, rerender } = renderHook(
+      ({ initialValue }) => useToggle(initialValue),
+      {
+        initialProps: { initialValue: false },
+      }
+    );
+
+    rerender({ initialValue: true });
+    act(() => result.current.onReset());
+    expect(result.current.isActive).toBe(true);
+  });
 });
